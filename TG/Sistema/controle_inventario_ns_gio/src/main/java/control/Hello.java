26ff2bd9290f5d5c73b440c1207a293b4a8cbf20
TiagoGomesTemoteo <tiago.gomes.temoteo@gmail.com>
2022-01-18 22:58:42 -0300

    Criação da e configuração da classe de conexão

--- TG/Sistema/controle_inventario_ns_gio/src/main/java/control/Hello.java
+++ TG/Sistema/controle_inventario_ns_gio/src/main/java/control/Hello.java
@@ -1,13 +1,17 @@
 package control;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import util.Conectar;
 
 /**
  *
@@ -19,10 +23,28 @@ public class Hello extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		out.println(request.getParameter("nome"));
-	}
+		
+		Connection conn = Conectar.getConnection();
+		
+		String imagem = request.getParameter("nome");
+        
+        String sql = "INSERT INTO IMAGENS (img_id, img_dt_cadastro, img_descricao)" +
+		"VALUES(img_id, now(), ?);";
+         
+        PreparedStatement stmt = null;
 
+		try{
+
+			stmt = conn.prepareStatement(sql);
+
+			stmt.setString(1,imagem);
+
+			stmt.executeUpdate();
+
+		}catch(SQLException ex){
+			System.out.println(ex.getMessage());
+		}
+	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
